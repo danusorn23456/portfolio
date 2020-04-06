82374bbc600cda236f2c39efddf2d4806f83ddfd
danusorn23456 <danusorn23456@gmail.com>
2020-04-06 18:49:57 +0700

    Updates

---
+++ precache-manifest.283ece3c4e3fadbcd724d795b86d32c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cb1b88ea0448906148b8f2dc3023eb8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "27a1112d7c53198eeb67",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a482bbf6dd5c06dc5d00",
+    "url": "/portfolio/static/js/2.4210c0ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.4210c0ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27a1112d7c53198eeb67",
+    "url": "/portfolio/static/js/main.ed4cdd11.chunk.js"
+  },
+  {
+    "revision": "1e74db4740f99c68a0bf",
+    "url": "/portfolio/static/js/runtime-main.b37861ed.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
