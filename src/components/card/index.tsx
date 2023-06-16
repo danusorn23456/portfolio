67c22d2648ee6844982faa520b396aa396134520
danusorn23456 <danusorn23456@gmail.com>
2023-06-16 16:54:31 +0700

    add css card props

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,15 +1,22 @@
 import styled from "styled-components";
-import { BackgroundProperties, EdgeProperties, SizingProperties } from "~/type";
+import {
+  BackgroundProperties,
+  EdgeProperties,
+  SizingProperties,
+  SpacingProperties,
+} from "~/type";
 import { mediaCSSBuilder } from "~/utils";
 
 export interface CardProps
   extends SizingProperties,
+    SpacingProperties,
     BackgroundProperties,
     EdgeProperties {}
 
 const Card = styled.div<CardProps>`
   ${(p) =>
     mediaCSSBuilder([
+      ["padding", p.padding || p.padding === 0 || "0.3rem", (v) => v],
       ["min-width", p.minWidth, (v) => v],
       ["width", p.width, (v) => v],
       ["max-width", p.maxWidth, (v) => v],
@@ -21,10 +28,9 @@ const Card = styled.div<CardProps>`
     !p.borderless &&
     mediaCSSBuilder([
       ["border-width", p.borderWidth || "1px", (v) => v],
-      ["border-color", p.borderWidth || "white", (v) => v],
+      ["border-color", p.borderWidth || "var(--black)", (v) => v],
       ["border-style", p.borderStyle || "solid", (v) => v],
     ])}
-  padding: ${(p) => (p.borderless ? 0 : 0.875)}rem;
   border-radius: 0.375rem;
   overflow: hidden;
   cursor: pointer;
