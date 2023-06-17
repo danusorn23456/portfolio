cc1a031f57cc10ff22840bc085b80269dedb118c
danusorn23456 <danusorn23456@gmail.com>
2023-06-17 12:11:44 +0700

    fix grid gap logic

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -14,7 +14,7 @@ const Grid = styled(Box)<GridProps>`
   display: grid;
   ${(p) =>
     mediaCSSBuilder([
-      ["gap", p.gap === "auto" && "2rem", (v) => v],
+      ["gap", p.gap === "auto" ? "2rem" : p.gap, (v) => v],
       ["grid-template-columns", p.cols, (v) => `repeat(${v},1fr)`],
     ])}
 `;
