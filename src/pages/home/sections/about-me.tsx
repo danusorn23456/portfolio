edbe4b92352557d58d1a9ad1a4762c597526a1cb
danusorn23456 <danusorn23456@gmail.com>
2023-06-18 02:15:37 +0700

    add social section

--- src/pages/home/sections/about-me.tsx
+++ src/pages/home/sections/about-me.tsx
@@ -1,11 +1,68 @@
-import { Card, Container, Image, Section, Stack, Text } from "~/components";
+import {
+  Box,
+  Card,
+  Container,
+  Image,
+  Section,
+  Stack,
+  Text,
+} from "~/components";
 import guitarImage from "../../../assets/guitar.jpeg";
+import { SiGmail, SiGithub } from "react-icons/si";
+import { useEffect, useRef } from "react";
+import { motion, useInView } from "framer-motion";
 
 export interface AboutMeProps {}
-
+const MotionBox = motion(Box);
 function AboutMe({}: AboutMeProps) {
+  const containerRef = useRef(null);
+  const isInView = useInView(containerRef);
+
+  useEffect(() => {}, [isInView]);
+
   return (
     <Section name="about-me">
+      <MotionBox
+        initial="hidden"
+        variants={{
+          hidden: {
+            opacity: 0,
+          },
+          visible: {
+            opacity: 1,
+          },
+        }}
+        animate={isInView ? "hidden" : "visible"}
+        position="fixed"
+        top={"2rem"}
+        left={0}
+        width="100%"
+        padding="0.7rem 0"
+        zIndex={1000}
+      >
+        <Stack
+          direction="row"
+          spaceX="0.5rem"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <a
+            target="_blank"
+            style={{
+              color: "var(--black)",
+              border: "1px solid var(--black-fade)",
+              borderTop: "none",
+              padding: "0.25rem 0.5rem",
+              backdropFilter: "blur(2px)",
+              borderBottomRightRadius: "0.5rem",
+              borderBottomLeftRadius: "0.5rem",
+            }}
+            href="mailto:danusorn23456@gmail.com"
+          >
+            danusorn23456@gmail.com
+          </a>
+        </Stack>
+      </MotionBox>
       <Container>
         <Stack
           height={"100vh"}
@@ -44,6 +101,53 @@ function AboutMe({}: AboutMeProps) {
                 actively engaged throughout the day.
               </Text>
             </Text>
+            <Stack margin="2rem 0 0 0" as="address">
+              <Text
+                as="p"
+                gradient={["var(--primary-base)", "var(--secondary-base)"]}
+              >
+                meet me at
+              </Text>
+              <Stack spaceY="0.5rem">
+                <Stack direction="row" spaceX="0.5rem">
+                  <span
+                    style={{
+                      fontSize: "1rem",
+                      color: "var(--black)",
+                      translate: "0 2px",
+                    }}
+                  >
+                    <SiGmail />
+                  </span>
+                  <a
+                    ref={containerRef}
+                    target="_blank"
+                    style={{ color: "var(--black)" }}
+                    href="mailto:danusorn23456@gmail.com"
+                  >
+                    danusorn23456@gmail.com
+                  </a>
+                </Stack>
+                <Stack direction="row" spaceX="0.5rem">
+                  <span
+                    style={{
+                      fontSize: "1rem",
+                      color: "var(--black)",
+                      translate: "0 2px",
+                    }}
+                  >
+                    <SiGithub />
+                  </span>
+                  <a
+                    target="_blank"
+                    style={{ color: "var(--black)" }}
+                    href="https://github.com/danusorn23456?tab=repositories"
+                  >
+                    danusorn23456
+                  </a>
+                </Stack>
+              </Stack>
+            </Stack>
           </Stack>
           <Stack alignItems="center" justifyContent="center" flex={1}>
             <Card padding={0}>
