d23c09ae57b613399fee1f3ec7a5649c73dad6e5
danusorn23456 <danusorn23456@gmail.com>
2023-06-18 14:49:33 +0700

    add padding top

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -46,7 +46,7 @@ function Layout({ children }: LayoutProps) {
           </Stack>
         </Container>
       </Box>
-      <Stack padding="0 0 33vh 0">{children}</Stack>
+      <Stack padding="1rem 0 33vh 0">{children}</Stack>
     </Page>
   );
 }
